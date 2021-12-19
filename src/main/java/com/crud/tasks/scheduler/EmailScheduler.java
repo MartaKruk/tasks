53175bef0e6272616e582a047004dbde137b4b50
Marta Kruk <kruk.marta@outlook.com>
2021-12-19 15:20:14 +0100

    23.4. Zadanie

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,36 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import com.crud.tasks.trello.config.AdminConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailScheduler {
+
+    private static final String SUBJECT = "Tasks: Once a day email";
+    private final SimpleEmailService simpleEmailService;
+    private final TaskRepository taskRepository;
+    private final AdminConfig adminConfig;
+
+    @Scheduled(cron = "0 0 10 * * *")
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        String message = "Currently in database you got " + size + " task";
+        if (size != 1) {
+            message += "s";
+        }
+        simpleEmailService.send(
+                new Mail(
+                        adminConfig.getAdminMail(),
+                        SUBJECT,
+                        message,
+                        null
+                )
+        );
+    }
+}
